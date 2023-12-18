00ba119878248e342f50b570c5009ee78001677f
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2023-12-18 17:31:54 -0500

    Added FactionNames and StarNames.

---
+++ Source/Factions/FactionNames.ts
@@ -0,0 +1,33 @@
+
+class FactionNames
+{
+	_All: string[]
+
+	constructor()
+	{
+		this._All =
+		[
+			"Astril",
+			"Bulvan",
+			"Drex",
+			"Eldor",
+			"Fulbi",
+			"Grast",
+			"Hemmeran",
+			"Ildun",
+			"Jarrae",
+			"Kwarg",
+			"Laretta",
+			"Mooldug",
+			"Opretti",
+			"Panquel",
+			"Restrin",
+			"Sivulan",
+			"Trontar",
+			"Ullex",
+			"Wolto",
+			"Yan",
+			"Zonnet"
+		];
+	}
+}
